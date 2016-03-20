5b22721954b0c74c3985564d650062f116d7d413
Michael Hamilton <michaelhamilton626@gmail.com>
2016-03-20 12:29:01 -0400

    Added a few basic routes for dealing with posts, as well as DB migrations for posts

--- Gemfile
+++ Gemfile
@@ -2,4 +2,8 @@
 source "https://rubygems.org"
 
 gem "sinatra"
+gem "sinatra-activerecord"
 gem "thin"
+gem "activerecord"
+gem "pg"
+gem "json"
--- app.rb
+++ app.rb
@@ -1,5 +1,27 @@
 require 'sinatra'
+require 'sinatra/activerecord'
+require './environments'
+require 'json'
 
-get '/' do
-  "Hello World!"
+class Post < ActiveRecord::Base
+end
+
+get "/posts" do
+  content_type :json
+  @posts = Post.order("created_at DESC")
+  @posts.to_json
+end
+
+post "/posts" do
+  content_type :json
+  @post = Post.new(params[:post])
+  if @post.save
+    @post.to_json
+  end
+end
+
+get "/posts/:id" do
+  content_type :json
+  @post = Post.find(params[:id])
+  @post.to_json
 end
